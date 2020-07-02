fa56dd807b28e63305a4bb864e3bb24368d0a452
Oshin-3 <oshin.gawande3@gmail.com>
2020-07-02 17:47:26 +0530

    jquery module

---
+++ js/index.js
@@ -0,0 +1,30 @@
+$(function(){
+	function hideAll(){
+		$(".containers").hide();
+	}
+
+	//hideAll();
+
+	function showLogin(){
+		hideAll();
+		$(".login-container").show();
+	}
+
+	//showLogin();
+
+	function showTable(){
+		hideAll();
+		$(".table-container").show();
+	}
+
+	//showTable();
+
+	function showForm(){
+		hideAll();
+		$(".form-container").show();
+	}
+
+	//showForm();
+
+	showLogin();
+});
