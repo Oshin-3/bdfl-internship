16a7781be84ae5d52700e1149a21b047b3ab89ab
Oshin-3 <oshin.gawande3@gmail.com>
2020-07-03 12:02:30 +0530

    sessionstorage

--- js/index.js
+++ js/index.js
@@ -1,35 +1,115 @@
-$(function(){
-	function hideAll(){
-		$(".containers").hide();
+var data = [
+  {
+    "requestId": 1,
+    "projectname": "Proj-1",
+    "projectmanager": "Man-1",
+    "resources":4,
+    "status":"open",
+    "poc":"HR-1",
+    "hiring-status":"completed",
+    "hr-omments":"ok",
+    "actions":"done"
+  },
+  {
+    "requestId": 2,
+    "projectname": "Proj-2",
+    "projectmanager": "Man-2",
+    "resources":14,
+    "status":"open",
+    "poc":"HR-2",
+    "hiring-status":"completed",
+    "hr-omments":"ok",
+    "actions":"done"
+  },
+  {
+    "requestId": 3,
+    "projectname": "Proj-3",
+    "projectmanager": "Man-3",
+    "resources":20,
+    "status":"open",
+    "poc":"HR-3",
+    "hiring-status":"completed",
+    "hr-omments":"ok",
+    "actions":"done"
+  }
+];
+
+function hideAll(){
+		$(".module").hide();
 	}
 
 	//hideAll();
 
-	function showLogin(){
-		hideAll();
-		$(".login-container").show();
+function showLogin(){
+	hideAll();
+	$(".login-module").show();
+}
+
+//showTable();
+
+function showForm(){
+	hideAll();
+	$(".form-module").show();
+}
+
+//showForm();
+function showDetails(){
+	hideAll();
+	$(".details-module").show();
+}
+
+function showTable(){
+	
+ 	hideAll();
+	$(".table-module").show();
+  for(let i=0;i<data.length;i++)
+  {
+    var rowData = data[i];
+    $("table tbody").append(`
+    		<tr>
+    			<td>${rowData["requestId"]}</td>
+    			<td>${rowData["projectname"]}</td>
+    			<td>${rowData["projectmanager"]}</td>
+    			<td>${rowData["resources"]}</td>
+    			<td>${rowData["status"]}</td>
+    			<td>${rowData["poc"]}</td>
+    			<td>${rowData["hiring-status"]}</td>
+    			<td>${rowData["hr-omments"]}</td>
+    			<td>${rowData["actions"]}</td>
+    		</tr>
+    	`);
+  }
+}
+
+function loginSubmit(e){
+	var username = $('#username').val();
+	var password = $('#password').val();
+
+	if (username == 'admin-man' && password == 'admin-man') {
+		sessionStorage.setItem("isManager", "true");
+		sessionStorage.setItem("username", "admin-man");
+		sessionStorage.setItem("password", "admin-man");
 	}
-
-	//showLogin();
-
-	function showTable(){
-		hideAll();
-		$(".table-container").show();
+	else{
+		sessionStorage.setItem("isHR", "true");
+		sessionStorage.setItem("username", "admin-hr");
+		sessionStorage.setItem("password", "admin-hr")
 	}
+}
 
-	//showTable();
 
-	function showForm(){
-		hideAll();
-		$(".form-container").show();
+function isLoggedIn(){
+	return (sessionStorage.getItem("username") == "admin-man" && sessionStorage.getItem("password") == "admin-man") || (sessionStorage.getItem("username") == "admin-hr" && sessionStorage.getItem("password") == "admin-hr");
+}
+
+$(".login-form").click(loginSubmit);
+
+$(function(){
+	if(isLoggedIn()){
+		showTable();
 	}
-
-	//showForm();
-	function showDetails(){
-		hideAll();
-		$(".details-container").show();
+	else{
+		showLogin();
 	}
-
-	//showDetails();
-	showLogin();
 });
+
