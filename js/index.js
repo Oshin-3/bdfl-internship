c3acbef156735256968c7622d823e69bf8478cf7
Oshin-3 <oshin.gawande3@gmail.com>
2020-07-07 14:22:10 +0530

    final frontend

--- js/index.js
+++ js/index.js
@@ -5,10 +5,22 @@ var data = [
     "projectname": "Proj-1",
     "projectmanager": "Man-1",
     "resources":4,
+    "designation": "xyz",
     "status":"open",
+    "minYear": 2,
+    "minMonth": 3,
+    "maxYear": 6,
+    "maxMonth": 2,
+    "type":"Replacement",
+    "skills":"Full Stack Developer",
+    "durationYear":1,
+    "durationMonth": 3,
+    "probability":"100%",
+    "salaryMin":"25K",
+    "salaryMax":"50K",
     "poc":"HR-1",
-    "hiring-status":"completed",
-    "hr-omments":"ok",
+    "hiringStatus":"completed",
+    "hrComments":"ok",
     "actions":"done"
   },
   {
@@ -16,10 +28,22 @@ var data = [
     "projectname": "Proj-2",
     "projectmanager": "Man-2",
     "resources":14,
-    "status":"open",
+    "designation": "mno",
+    "status":"new",
+    "minYear": 2,
+    "minMonth": 3,
+    "maxYear": 5,
+    "maxMonth": 2,
+    "type":"New",
+    "skills":"Full Stack Developer",
+    "durationYear":1,
+    "durationMonth": 2,
+    "probability":"80%",
+    "salaryMin":"25K",
+    "salaryMax":"50K",
     "poc":"HR-2",
-    "hiring-status":"completed",
-    "hr-omments":"ok",
+    "hiringStatus":"completed",
+    "hrComments":"ok",
     "actions":"done"
   },
   {
@@ -27,10 +51,22 @@ var data = [
     "projectname": "Proj-3",
     "projectmanager": "Man-3",
     "resources":20,
+    "designation": "abc",
     "status":"open",
+    "minYear": 2,
+    "minMonth": 4,
+    "maxYear": 4,
+    "maxMonth": 2,
+    "type":"New",
+    "skills":"Full Stack Developer",
+    "durationYear":0,
+    "durationMonth": 8,
+    "probability":"100%",
+    "salaryMin":"25K",
+    "salaryMax":"50K",
     "poc":"HR-3",
-    "hiring-status":"completed",
-    "hr-omments":"ok",
+    "hiringStatus":"completed",
+    "hrComments":"ok",
     "actions":"done"
   }
 ];
@@ -78,7 +114,9 @@ function editDetails(e){
     $details.addClass('details-copy').show();
     $new_tr.find('td').append($details);
 
-    //$('label.projectname').text(selReqObj.projectname);
+    setDetailsValues();
+
+    // $('label.projectname').text(selReqObj.projectname);
     $details.find('.clone-form').click(function(){
       closeDetails();
       $new_tr.find('.details-module').hide();
@@ -86,6 +124,7 @@ function editDetails(e){
       var $formCopy = $('.form-module').clone();
       $formCopy.addClass('form-copy').show();
       $new_tr.find('td').append($formCopy);
+      setEditFormValues();
     })
 };
 
@@ -113,8 +152,8 @@ function showTable(){
                 <td>${rowData["resources"]}</td>
                 <td>${rowData["status"]}</td>
                 <td>${rowData["poc"]}</td>
-                <td>${rowData["hiring-status"]}</td>
-                <td>${rowData["hr-omments"]}</td>
+                <td>${rowData["hiringStatus"]}</td>
+                <td>${rowData["hrComments"]}</td>
                 <td id="${rowData["requestId"]}" >
                   <i class="fa fa-bars edit-action"></i>
                   <i class="fa fa-times close-action"></i>
@@ -139,6 +178,46 @@ function handleIcons($req){
 
   closeDetails();
 };
+
+function setDetailsValues(){
+  $('label.projectname').text(selReqObj.projectname);
+  $('label.projectmanager').text(selReqObj.projectmanager);
+  $('label.resources').text(selReqObj.resources);
+  $('label.designation').text(selReqObj.designation);
+  $('label.status').text(selReqObj.status);
+  $('label.min-experience').text(selReqObj.minYear+ " Years & " + selReqObj.minMonth + " Months");
+  $('label.max-experience').text(selReqObj.maxYear+ " Years & " + selReqObj.maxMonth + " Months");
+  $('label.type').text(selReqObj.type);
+  $('label.skills').text(selReqObj.skills);
+  $('label.duration').text(selReqObj.durationYear + " Years & " + selReqObj.durationMonth + " Months");
+  $('label.probability').text(selReqObj.probability);
+  $('label.salary').text(selReqObj.salaryMin + " Min. & " + selReqObj.salaryMax + " Max.");
+  $('label.poc').text(selReqObj.poc);
+  $('label.hirirng-status').text(selReqObj.hiringStatus);
+  $('label.hr-comments').text(selReqObj.hrComments);
+}
+
+function setEditFormValues() {
+  $('#projectname').val(selReqObj.projectname);
+  $('#projectmanager').val(selReqObj.projectmanager);
+  $('#resources').val(selReqObj.resources);
+  $('#designation').val(selReqObj.designation);
+  $('#status').val(selReqObj.status);
+  $('#minYear').val(selReqObj.minYear);
+  $('#minMonth').val(selReqObj.minMonth);
+  $('#maxYear').val(selReqObj.maxYear);
+  $('#maxMonth').val(selReqObj.maxMonth);
+  $('#type').val(selReqObj.type);
+  $('#skills').val(selReqObj.skills);
+  $('#durationYear').val(selReqObj.durationYear);
+  $('#durationMonth').val(selReqObj.durationMonth);
+  $('#conversion').val(selReqObj.probability);
+  $('#salaryMin').val(selReqObj.salaryMin);
+  $('#salaryMax').val(selReqObj.salaryMax);
+  $('#poc').val(selReqObj.poc);
+  $('#hiring-status').val(selReqObj.hiringStatus);
+  $('#hr-comments').val(selReqObj.hrComments);
+}
 
 function loginSubmit(e){
 	var username = $('#username').val();
